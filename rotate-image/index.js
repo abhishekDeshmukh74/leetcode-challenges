c05f0788e6473a814f5bf51c513f271b1bddeab3
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-06-26 23:20:07 +0530

    rotate-image

---
+++ rotate-image/index.js
@@ -0,0 +1,35 @@
+var rotate = function (matrix) {
+  let l = 0;
+  let r = matrix.length - 1;
+
+  while (l < r) {
+    for (let i = 0; i < r - l; i++) {
+      // store top left
+      const topLeft = matrix[l][l + i];
+
+      // top left <== bottom left
+      matrix[l][l + i] = matrix[r - i][l];
+
+      // bottom left <== bottom right
+      matrix[r - i][l] = matrix[r][r - i];
+
+      // bottom right <== top right
+      matrix[r][r - i] = matrix[l + i][r];
+
+      // top right <== top left
+      matrix[l + i][r] = topLeft;
+    }
+    l++;
+    r--;
+  }
+  return matrix;
+};
+
+console.log(
+  rotate([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16],
+  ])
+);
