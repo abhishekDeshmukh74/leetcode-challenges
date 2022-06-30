1a64efd0674d2181f2a9ea0f1f596c74dd24819d
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-01 00:55:26 +0530

    binary-search

---
+++ binary-search/index.js
@@ -0,0 +1,19 @@
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let pivot = Math.floor((left + right) / 2);
+    if (nums[pivot] === target) return pivot;
+    if (nums[pivot] > target) {
+      right = pivot - 1;
+    } else {
+      left = pivot + 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
+console.log(search([5], 5));
