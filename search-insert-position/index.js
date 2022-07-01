07b2631d479c7a4fab3a63592c618fc9e6319a83
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-01 22:02:33 +0530

    searchInsert

---
+++ search-insert-position/index.js
@@ -0,0 +1,17 @@
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (nums[middle] >= target) {
+      if (nums[middle] === target) return middle;
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return left;
+};
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
