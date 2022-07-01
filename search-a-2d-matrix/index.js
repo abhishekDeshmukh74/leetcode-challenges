10fcb56e7032766af77178fda3285bc4387fe716
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-01 18:57:38 +0530

    searchMatrix

---
+++ search-a-2d-matrix/index.js
@@ -0,0 +1,28 @@
+var searchMatrix = function (matrix, target) {
+  let left = 0;
+  let right = matrix.length * matrix[0].length - 1;
+
+  while (left <= right) {
+    let middle = Math.ceil((left + right) / 2);
+    const middleI = Math.floor(middle / matrix[0].length);
+    const middleJ = middle % matrix[0].length;
+    if (matrix[middleI][middleJ] >= target) {
+      if (matrix[middleI][middleJ] === target) return true;
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return false;
+};
+
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    3
+  )
+);
