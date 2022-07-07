b75e6778283eb61f2d65e4849b5adeebeeee627d
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-07 15:57:19 +0530

    mergeTrees

--- merge-two-binary-trees/index.js
+++ merge-two-binary-trees/index.js
@@ -22,16 +22,27 @@ root2.right = node3_2;
 node1.right = node4;
 node3_2.right = node7;
 
-var mergeTrees = function (root1, root2) {
-  if (!root1 && !root2) return null;
-  if (!root1 && root2) return root2;
-  if (root1 && !root2) return root1;
+// Create new merged tree
+// var mergeTrees = function (root1, root2) {
+//   if (!root1 && !root2) return null;
+//   if (!root1 && root2) return root2;
+//   if (root1 && !root2) return root1;
 
-  const root = new TreeNode(root1.val + root2.val);
+//   const root = new TreeNode(root1.val + root2.val);
 
-  root.left = mergeTrees(root1.left, root2.left);
-  root.right = mergeTrees(root1.right, root2.right);
-  return root;
+//   root.left = mergeTrees(root1.left, root2.left);
+//   root.right = mergeTrees(root1.right, root2.right);
+//   return root;
+// };
+
+// Merge on root1
+let mergeTrees = function (root1, root2) {
+  if (!root1) return root2;
+  if (!root2) return root1;
+  root1.val += root2.val;
+  root1.left = mergeTrees(root1.left, root2.left);
+  root1.right = mergeTrees(root1.right, root2.right);
+  return root1;
 };
 
 console.log(mergeTrees(root1, root2));
