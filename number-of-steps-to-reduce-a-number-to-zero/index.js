671aebcadfcee3c94a247ab97077a292a046b26e
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-09 02:56:14 +0530

    numberOfSteps

---
+++ number-of-steps-to-reduce-a-number-to-zero/index.js
@@ -0,0 +1,9 @@
+var numberOfSteps = function (num) {
+  let steps = 0;
+  while (num !== 0) {
+    num % 2 === 0 ? (num /= 2) : (num -= 1);
+    steps++;
+  }
+  return steps;
+};
+console.log(numberOfSteps(14));
