d7c991f28270122235494197529d999b80eec06e
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-12 00:04:50 +0530

    rangeSumBST

---
+++ range-sum-of-bst/index.js
@@ -0,0 +1,30 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const root = new TreeNode(10);
+const node5 = new TreeNode(5);
+const node15 = new TreeNode(15);
+const node3 = new TreeNode(3);
+const node7 = new TreeNode(7);
+const node18 = new TreeNode(18);
+
+root.left = node5;
+root.right = node15;
+node5.left = node3;
+node5.right = node7;
+node15.right = node18;
+
+// Recursive BST
+var rangeSumBST = function (root, low, high) {
+  if (!root) return 0;
+  let sum = root.val;
+  if (root.val < low) sum = 0;
+  if (root.val > high) sum = 0;
+
+  return sum + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+};
+
+console.log(rangeSumBST(root, 7, 15));
