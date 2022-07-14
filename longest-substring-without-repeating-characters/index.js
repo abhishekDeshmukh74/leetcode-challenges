de12f1c4e86055a1131a71f15f3c8eb2f519604a
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-14 13:23:20 +0530

    lengthOfLongestSubstring

---
+++ longest-substring-without-repeating-characters/index.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function (s) {
+  const set = new Set();
+
+  let maxLength = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (set.has(s[i])) {
+      const currentSize = set.size;
+      if (currentSize > maxLength) maxLength = currentSize;
+      set.clear();
+      i -= currentSize - 1;
+    }
+    set.add(s[i]);
+  }
+  return Math.max(maxLength, set.size);
+};
+console.log(lengthOfLongestSubstring('abcabcbb'));
+console.log(lengthOfLongestSubstring(' '));
+console.log(lengthOfLongestSubstring('aab'));
+console.log(lengthOfLongestSubstring('dvdf'));
