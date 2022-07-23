ad55920fda2ee2400a2be2aca04d4ce13dd3fc16
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-24 05:10:34 +0530

    setZeroes

---
+++ set-matrix-zeroes/index.js
@@ -0,0 +1,33 @@
+var setZeroes = function (matrix) {
+
+    let rowZero = false;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[0][j] = 0;
+                i > 0 ? matrix[i][0] = 0 : rowZero = true;
+            }
+        }
+    }
+
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[i].length; j++) {
+            if (matrix[0][i] === 0 || matrix[0][j] === 0) matrix[i][j] = 0
+        }
+    }
+
+    if (matrix[0][0] === 0) {
+        for (let i = 0; i < matrix.length; i++) matrix[i][0] = 0
+    }
+    if (rowZero) {
+        for (let j = 0; j < matrix[0].length; j++) matrix[0][j] = 0
+    }
+    return matrix
+};
+
+console.log(setZeroes([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+]))
