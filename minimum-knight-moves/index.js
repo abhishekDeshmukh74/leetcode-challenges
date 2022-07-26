f9393505d5e0a53a6e93fcf048abbd283b45f261
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-07-26 10:02:46 +0530

    minKnightMoves

---
+++ minimum-knight-moves/index.js
@@ -0,0 +1,21 @@
+var minKnightMoves = function (x, y) {
+
+    const knightDirections = [[2, 1], [-2, 1], [2, -1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
+    const visited = new Set();
+    const queue = [[0, 0, 0]]
+
+    while (queue.length) {
+
+        const [currentX, currentY, currentDistance] = queue.shift();
+        if (currentX === x && currentY === y) return currentDistance;
+
+        for (const [dx, dy] of knightDirections) {
+            const newX = currentX + dx;
+            const newY = currentY + dy;
+            if (visited.has(`${newX}-${newY}`)) continue
+
+            visited.add(`${newX}-${newY}`)
+            queue.push([newX, newY, currentDistance + 1])
+        }
+    }
+};
