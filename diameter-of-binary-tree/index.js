376f5c83b5d2adaf661438185715ec743f84ec6c
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-08-05 12:51:11 +0530

    diameterOfBinaryTree

---
+++ diameter-of-binary-tree/index.js
@@ -0,0 +1,30 @@
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+
+const root = new TreeNode(1);
+const node2 = new TreeNode(2);
+const node3 = new TreeNode(3);
+const node4 = new TreeNode(4);
+const node5 = new TreeNode(5);
+root.left = node2;
+root.right = node3;
+node2.left = node4;
+node2.right = node5;
+
+// Recursive Top-Down will have O(n^2), but recursive bottom-up will have O(n)
+var diameterOfBinaryTree = function (root, max = 0) {
+    const dfs = (root) => {
+        if (!root) return -1
+        const left = dfs(root.left)
+        const right = dfs(root.right)
+        max = Math.max(max, left + right + 2)
+        return 1 + Math.max(left, right)
+    }
+    dfs(root)
+    return max
+};
+
+console.log(diameterOfBinaryTree(root))
