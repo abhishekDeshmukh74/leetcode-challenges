09eecd422274d34b3e859f9fc9971562b230b448
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-08-09 12:48:25 +0530

    shortestCommonSupersequence

---
+++ shortest-common-supersequence/index.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var shortestCommonSupersequence = function (str1, str2) {
+
+    const table = Array(str1.length + 1).fill().map(() => Array(str2.length + 1).fill(0))
+
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                table[i][j] = 1 + table[i - 1][j - 1]
+            } else {
+                table[i][j] = Math.max(table[i][j - 1], table[i - 1][j])
+            }
+        }
+    }
+
+    let superStr = '';
+    let i = str1.length
+    let j = str2.length
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            superStr += str1[i - 1]
+            i--
+            j--
+        } else {
+            if (table[i][j - 1] > table[i - 1][j]) {
+                superStr += str2[j - 1]
+                j--
+            } else {
+                superStr += str1[i - 1]
+                i--
+            }
+        }
+    }
+
+    while (i > 0) {
+        superStr += str1[i - 1]
+        i--
+    }
+    while (j > 0) {
+        superStr += str2[j - 1];
+        j--;
+    }
+    return superStr.split('').reverse().join('')
+};
