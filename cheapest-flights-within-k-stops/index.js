17fa4ecb8ccc2be3b934ec2f3428ac81d894c1b3
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-08-18 03:35:25 +0530

    upd

--- cheapest-flights-within-k-stops/index.js
+++ cheapest-flights-within-k-stops/index.js
@@ -5,16 +5,14 @@ var findCheapestPrice = function (n, flights, src, dst, k) {
 
     for (let i = 0; i < k + 1; i++) {
         const tempPrices = [...prices]
-        for (const flight of flights) {
-            const [s, d, p] = flight;
-            if (prices[s] === Infinity) continue;
-            const possibleCheapPrice = prices[s] + p
-            if (possibleCheapPrice < tempPrices[d]) tempPrices[d] = possibleCheapPrice;
+
+        for (const [s, d, w] of flights) {
+            if (prices[s] === Infinity) continue
+            const currentPrice = prices[s] + w
+            if (currentPrice < tempPrices[d]) tempPrices[d] = currentPrice
         }
         prices = tempPrices
     }
-    if (prices[dst] === Infinity) return -1;
-    return prices[dst]
+    return prices[dst] === Infinity ? -1 : prices[dst]
 };
-
 console.log(findCheapestPrice(4, [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]], 0, 3, 1))
