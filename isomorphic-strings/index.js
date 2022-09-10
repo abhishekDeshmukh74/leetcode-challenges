634b6e17e0b1cadcd80b817d1355f8a15e590ab5
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-09-11 02:28:33 +0530

    isIsomorphic

---
+++ isomorphic-strings/index.js
@@ -0,0 +1,19 @@
+var isIsomorphic = function (s, t) {
+    const mapS = {}
+    const mapT = {}
+
+    for (let i = 0; i < s.length; i++) {
+        const c1 = s[i]
+        const c2 = t[i]
+
+        // if mapping doesn't exist
+        if (!mapS[c1] && !mapT[c2]) {
+            mapS[c1] = c2
+            mapT[c2] = c1
+            continue
+        }
+        // mapping doesn't exist in one of the dictionaries or Mapping exists and it doesn't match in either of the dictionaries or both
+        if (!(mapS[c1] == c2 && mapT[c2] == c1)) return false
+    }
+    return true
+};
