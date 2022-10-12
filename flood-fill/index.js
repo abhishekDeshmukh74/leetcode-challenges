862e9a9e9ae47055dd9f64d4672cb0d62e705bf1
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-10-13 01:46:48 +0530

    floodFill opt

--- flood-fill/index.js
+++ flood-fill/index.js
@@ -1,4 +1,3 @@
-// TODO - Optimize
 var floodFill = function (image, sr, sc, color) {
   const directions = [
     [0, 1],
@@ -6,51 +5,26 @@ var floodFill = function (image, sr, sc, color) {
     [0, -1],
     [-1, 0],
   ];
-
-  const visited = new Set();
-  const flooded = new Set();
-  const queue = [[sr, sc]];
+  const queue = [[sr, sc]]
+  const startingPixelColor = image[sr][sc]
+  image[sr][sc] = color
 
   while (queue.length) {
-    const [x, y] = queue.shift();
-    const key = `${x}-${y}`;
-    visited.add(key);
-    flooded.add(key);
+    const [x, y] = queue.shift()
 
     for (const [dx, dy] of directions) {
-      const newX = x + dx;
-      const newY = y + dy;
-      const xInbounds = newX >= 0 && newX < image.length;
-      const yInbounds = newY >= 0 && newY < image[0].length;
-      if (
-        !xInbounds ||
-        !yInbounds ||
-        image[newX][newY] !== image[x][y] ||
-        visited.has(`${newX}-${newY}`)
-      )
-        continue;
+      const newX = x + dx
+      const newY = y + dy
+      if (newX < 0 || newY < 0 || newX >= image.length || newY >= image[0].length) continue;
+      if (image[newX][newY] !== startingPixelColor) continue;
+      if (startingPixelColor === color) continue;
 
-      flooded.add(`${newX}-${newY}`);
+      image[newX][newY] = color
       queue.push([newX, newY]);
     }
   }
+  return image
+}
 
-  for (const [entry] of flooded.entries()) {
-    const [x, y] = entry.split('-');
-    image[x][y] = color;
-  }
-  return image;
-};
-
-console.log(
-  floodFill(
-    [
-      [1, 1, 1],
-      [1, 1, 0],
-      [1, 0, 1],
-    ],
-    1,
-    1,
-    2
-  )
-);
+console.log(floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2))
+console.log(floodFill([[0, 0, 0], [0, 0, 0]], 0, 0, 0))
