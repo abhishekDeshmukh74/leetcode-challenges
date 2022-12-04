8aa8edb89de5aca452260357e86a498d8a64fc81
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-12-04 23:58:31 +0530

    countBits

---
+++ counting-bits/index.js
@@ -0,0 +1,11 @@
+// for odd num i => no of 1s(i) = 1 + no of 1s(i/2) and for even num i=> no of 1s(i) = no of 1s(i/2)
+var countBits = function (n) {
+    const table = [0]
+    for (let i = 1; i <= n; i++) {
+        table[i] = table[Math.floor(i / 2)] + i % 2
+    }
+    return table
+};
+
+console.log(countBits(2))
+console.log(countBits(5))
