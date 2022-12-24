fb4cbf600ec472733c798e0ffa465287d58a2531
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-12-25 01:12:35 +0530

    k-closest-points-to-origin

---
+++ k-closest-points-to-origin/index.js
@@ -0,0 +1,18 @@
+const {
+    MaxPriorityQueue,
+} = require('@datastructures-js/priority-queue');
+
+var kClosest = function (points, k) {
+
+    if (points.length === k) return points
+    const distances = points.map(([x, y]) => Math.sqrt(x * x + y * y))
+
+    const maxHeap = new MaxPriorityQueue()
+    for (let i = 0; i < distances.length; i++) {
+        maxHeap.enqueue(i, distances[i])
+        while (maxHeap.size() > k) maxHeap.dequeue()
+    }
+    return maxHeap.toArray().map(x => points[x.element])
+};
+
+console.log(kClosest([[1, 3], [-2, 2]], 1))
