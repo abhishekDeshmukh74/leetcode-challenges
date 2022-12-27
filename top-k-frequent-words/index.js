30fbdb681a81fe17ff052b9167750232c473ed58
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-12-28 03:21:47 +0530

    topKFrequent

---
+++ top-k-frequent-words/index.js
@@ -0,0 +1,29 @@
+const {
+    MinPriorityQueue,
+} = require('@datastructures-js/priority-queue')
+
+var topKFrequent = function (words, k) {
+    const map = {}
+    words.forEach(word => map[word] ? map[word]++ : map[word] = 1)
+
+    const minHeap = new MinPriorityQueue({
+        compare: (w1, w2) => {
+            // if count is the same, compare string(length and alphabetic order)
+            if (map[w1] === map[w2]) {
+                if (w1 === w2) return 0
+                if (w1 < w2) return 1
+                return -1
+            }
+            return map[w1] < map[w2] ? -1 : 1
+        }
+    })
+
+    for (const key in map) {
+        minHeap.enqueue(key, map[key])
+        if (minHeap.size() > k) minHeap.dequeue()
+    }
+    return minHeap.toArray().reverse()
+}
+
+console.log(topKFrequent(['i', 'love', 'leetcode', 'i', 'love', 'coding'], 2))
+console.log(topKFrequent(['the', 'day', 'is', 'sunny', 'the', 'the', 'the', 'sunny', 'is', 'is'], 4))
