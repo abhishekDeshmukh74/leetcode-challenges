ec190c8de8077e3843c959fba66922e0a3d84f83
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-12-29 16:47:52 +0530

    frequencySort

---
+++ sort-characters-by-frequency/index.js
@@ -0,0 +1,39 @@
+// HashMap and Sort
+// Time complexity: O(NlogN)
+// Space complexity: O(N)
+var frequencySort = function (s) {
+    const map = {}
+    for (const c of s) map[c] ? map[c]++ : map[c] = 1
+    const sortedChars = Object.keys(map).sort((a, b) => map[b] - map[a])
+
+    const result = []
+    sortedChars.forEach(char => {
+        let occurrences = map[char]
+        while (occurrences--) result.push(char)
+    })
+    return result.join('')
+}
+
+// HashMap and Bucket sort
+// Time complexity: O(N)
+// Space complexity: O(N)
+var frequencySort = function (s) {
+    const map = new Map()
+    const bucket = []
+    let output = ''
+
+    for (let c of s) map.set(c, (map.get(c) || 0) + 1)
+
+    for (let [char, freq] of map) {
+        if (!bucket[freq]) bucket[freq] = []
+        bucket[freq].push(char)
+    }
+
+    for (let i = bucket.length - 1; i >= 0; i--) {
+        if (!bucket[i]) continue;
+        for (let char of bucket[i]) output += char.repeat(i)
+    }
+    return output
+}
+
+console.log(frequencySort('tree'))
