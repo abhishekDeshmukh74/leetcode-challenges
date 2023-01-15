989c8d290a3976ca3d0188fb12909e5841684e00
abhishek.d3 <abhishek.d@affinidi.com>
2023-01-15 14:34:22 +0530

    Quick select findKthLargest

--- kth-largest-element-in-an-array/index.js
+++ kth-largest-element-in-an-array/index.js
@@ -1,14 +1,39 @@
-const {
-    MinPriorityQueue,
-} = require('@datastructures-js/priority-queue')
+const { MinPriorityQueue } = require("@datastructures-js/priority-queue")
 
+// Heap
+// Time complexity: O(Nlogk)
 var findKthLargest = function (nums, k) {
-    const minHeap = new MinPriorityQueue()
-    for (const num of nums) {
-        minHeap.enqueue(num)
-        if (minHeap.size() > k) minHeap.dequeue()
+  const minHeap = new MinPriorityQueue()
+  for (const num of nums) {
+    minHeap.enqueue(num)
+    if (minHeap.size() > k) minHeap.dequeue()
+  }
+  return minHeap.front().element
+}
+
+// Quick select
+// Time complexity: O(n) average case, O(n2) worst case
+var findKthLargest = function (nums, k) {
+  const quickSelect = (left, right) => {
+    const pivot = nums[right]
+    let p = left
+
+    for (let i = left; i < right; i++) {
+      if (nums[i] <= pivot) {
+        [nums[i], nums[p]] = [nums[p], nums[i]]
+        p++
+      }
     }
-    return minHeap.front().element
+    [nums[p], nums[right]] = [nums[right], nums[p]]
+
+    if (p === nums.length - k) return nums[p]
+    if (p > nums.length - k) {
+      return quickSelect(left, p - 1);
+    } else {
+      return quickSelect(p + 1, right);
+    }
+  }
+  return quickSelect(0, nums.length - 1)
 }
 
 console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2))
