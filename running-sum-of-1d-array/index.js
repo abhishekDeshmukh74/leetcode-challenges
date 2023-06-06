8afa4ce26d49a4ff32788e28a8cbba076b4280e7
abhishek.d3 <abhishek.d@affinidi.com>
2023-06-06 13:31:13 +0530

    runningSum

--- running-sum-of-1d-array/index.js
+++ running-sum-of-1d-array/index.js
@@ -1,10 +1,7 @@
 var runningSum = function (nums) {
-  const prefix = [nums[0]];
-  let sum = nums[0];
   for (let i = 1; i < nums.length; i++) {
-    prefix.push(sum + nums[i]);
-    sum = sum + nums[i];
+    nums[i] += nums[i - 1];
   }
-  return prefix;
+  return nums;
 };
 console.log(runningSum([1, 2, 3, 4]));
