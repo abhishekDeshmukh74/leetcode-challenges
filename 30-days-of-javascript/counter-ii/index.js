fe5dcd43acea0944a1bb7d8064643def89cac440
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2023-08-08 23:11:28 +0530

    createCounter

---
+++ 30-days-of-javascript/counter-ii/index.js
@@ -0,0 +1,13 @@
+var createCounter = function (init) {
+    let presentCount = init
+    return {
+        increment: () => ++presentCount,
+        decrement: () => --presentCount,
+        reset: () => presentCount = init,
+    }
+};
+
+const counter = createCounter(5)
+counter.increment(); // 6
+counter.reset(); // 5
+counter.decrement(); // 4
