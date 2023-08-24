5d9081a93468dc3db7306ad37a447f375b8226dc
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2023-08-24 17:43:47 +0530

    fib update

--- fibonacci-number/index.js
+++ fibonacci-number/index.js
@@ -24,7 +24,7 @@ var fib = function (n, memo = {}) {
 var fib = function (n) {
   const table = Array(n + 1).fill(0);
   table[1] = 1;
-  for (let i = 0; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {
     table[i + 1] += table[i];
     table[i + 2] += table[i];
   }
