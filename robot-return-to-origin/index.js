1ffe5962827cc6023c5fb0d3c36e2023d4b04c5b
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-04-01 13:43:34 +0530

    robot-return-to-origin

---
+++ robot-return-to-origin/index.js
@@ -0,0 +1,14 @@
+var judgeCircle = function (moves) {
+    let x = 0;
+    let y = 0;
+    for (const move of moves) {
+        if (move === 'U') y++;
+        if (move === 'D') y--;
+        if (move === 'L') x--;
+        if (move === 'R') x++;
+    }
+    return x === 0 && y === 0
+};
+
+console.log(judgeCircle("UD"))
+console.log(judgeCircle("LL"))
