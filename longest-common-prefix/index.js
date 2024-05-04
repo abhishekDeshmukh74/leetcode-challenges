6fb795bedf8a320c5220e9ec9ce2d38f02733887
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-05-04 17:41:04 +0530

    Longest Common Prefix

---
+++ longest-common-prefix/index.js
@@ -0,0 +1,14 @@
+var longestCommonPrefix = function (strs) {
+    let prefix = ""
+
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== strs[0][i]) return prefix
+        }
+        prefix += strs[0][i]
+    }
+    return prefix
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
+console.log(longestCommonPrefix(["dog", "racecar", "car"]))
