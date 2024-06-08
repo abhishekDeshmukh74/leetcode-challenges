15daf8d40773642012367b00084aa493cd4587d7
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-06-08 12:52:43 +0530

    hammingWeight

--- number-of-1-bits/index.js
+++ number-of-1-bits/index.js
@@ -1,9 +1,10 @@
 var hammingWeight = function (n) {
   let count = 0;
   while (n !== 0) {
-    count += n & 1;
-    n >>>= 1;
+    count += n & 1; // Increment count if the least significant bit is 1
+    n >>= 1; // Unsigned right shift by 1
   }
   return count;
 };
-console.log(hammingWeight(00000000000000000000000000001011));
+
+console.log(hammingWeight(11));
