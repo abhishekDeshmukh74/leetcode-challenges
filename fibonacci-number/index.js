1f2d0834ad4b3eee8b204fd405767b9f2f9c7c5c
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-06-21 23:47:03 +0530

    fibonacci-number

--- fibonacci-number/index.js
+++ fibonacci-number/index.js
@@ -1,22 +1,15 @@
 // Recursive non-memoized approach - Time complexity => O(2^n) - Space complexity => O(n)
 var fib = function (n) {
   if (n === 0 || n === 1) return n;
-  if (n < 0) return 0;
-
-  for (let i = 2; i <= n; i++) {
-    return fib(i - 1, memo) + fib(i - 2, memo);
-  }
+  return fib(n - 1) + fib(n - 2);
 };
 
 // Recursive memoized approach - Time complexity => O(n) - Space complexity => O(n)
 var fib = function (n, memo = {}) {
-  if (memo[n]) return memo[n];
   if (n === 0 || n === 1) return n;
-  if (n < 0) return 0;
+  if (memo[n]) return memo[n];
 
-  for (let i = 2; i <= n; i++) {
-    memo[n] = fib(i - 1, memo) + fib(i - 2, memo);
-  }
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
   return memo[n];
 };
 
@@ -24,11 +17,23 @@ var fib = function (n, memo = {}) {
 var fib = function (n) {
   const table = Array(n + 1).fill(0);
   table[1] = 1;
-  for (let i = 1; i <= n; i++) {
-    table[i + 1] += table[i];
-    table[i + 2] += table[i];
+  for (let i = 2; i <= n; i++) {
+    table[i] = table[i - 1] + table[i - 2]
   }
   return table[n];
+};
+
+// Iterative approach - Time complexity => O(n) - Space complexity => O(1)
+var fib = function (n) {
+  let secondLast = 0
+  let last = 1
+
+  for (let i = 2; i <= n; i++) {
+    const current = last + secondLast
+    secondLast = last
+    last = current
+  }
+  return last;
 };
 
 console.log(fib(6));
