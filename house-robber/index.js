2974fab8f98fec5785da91801cfc19e804349eec
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-06-22 18:36:39 +0530

    House Robber opti

--- house-robber/index.js
+++ house-robber/index.js
@@ -1,24 +1,20 @@
-// Recursive - fails for large array sizes due to slice
-const rob = (nums, memo = {}) => {
-  if (memo[nums.length]) return memo[nums.length];
-  if (nums.length === 1) memo[nums.length] = nums[0];
-  if (nums.length === 2) memo[nums.length] = Math.max(nums[0], nums[1]);
+// Recursive
+var rob = (nums, i = nums.length - 1, memo = {}) => {
+  if (i in memo) return memo[i];
+  if (i === 0) return nums[0];
+  if (i === 1) return Math.max(nums[0], nums[1]);
 
-  for (let i = 2; i < nums.length; i++) {
-    memo[nums.length] = Math.max(
-      nums[i] + rob(nums.slice(0, i - 1), memo),
-      rob(nums.slice(0, i), memo)
-    );
-  }
-  return memo[nums.length];
+  memo[i] = Math.max(
+    nums[i] + rob(nums, i - 2, memo),
+    rob(nums, i - 1, memo)
+  );
+
+  return memo[i];
 };
 
 // Iterative
-const rob = (nums) => {
-  if (nums.length === 1) return nums[0];
-  if (nums.length === 2) return Math.max(nums[0], nums[1]);
-
-  dp = [nums[0], Math.max(nums[0], nums[1])];
+var rob = (nums) => {
+  const dp = [nums[0], Math.max(nums[0], nums[1] || 0)];
 
   for (let i = 2; i < nums.length; i++) {
     dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
@@ -26,7 +22,9 @@ const rob = (nums) => {
   return dp.pop();
 };
 
+
 console.log(rob([1, 2, 3, 1]));
+console.log(rob([2, 7, 9, 3, 1]));
 console.log(
   rob([
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
