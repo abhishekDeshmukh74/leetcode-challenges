d1c67a8701a91d424c5656b90f138fc9ac099ca4
Ragini Pandey <pandeyragini55@gmail.com>
2024-07-28 23:46:15 +0530

    insertIntoBST

--- insert-into-a-binary-search-tree/index.js
+++ insert-into-a-binary-search-tree/index.js
@@ -14,28 +14,6 @@ root.right = node7;
 node2.left = node1;
 node2.right = node3;
 
-// var insertIntoBST = function (root, val) {
-//   const node = new TreeNode(val);
-//   if (!root) return node;
-
-//   let current = root;
-//   while (current) {
-//     if (val > current.val) {
-//       if (!current.right) {
-//         current.right = node;
-//         return root;
-//       }
-//       current = current.right;
-//     } else {
-//       if (!current.left) {
-//         current.left = node;
-//         return root;
-//       }
-//       current = current.left;
-//     }
-//   }
-// };
-
 // Iterative way
 var insertIntoBST = function (root, val) {
   const newNode = new TreeNode(val);
@@ -59,6 +37,28 @@ var insertIntoBST = function (root, val) {
     }
   }
   return root;
+};
+
+var insertIntoBST = function (root, val) {
+  const node = new TreeNode(val);
+  if (!root) return node;
+
+  let current = root;
+  while (current) {
+    if (val > current.val) {
+      if (!current.right) {
+        current.right = node;
+        return root;
+      }
+      current = current.right;
+    } else {
+      if (!current.left) {
+        current.left = node;
+        return root;
+      }
+      current = current.left;
+    }
+  }
 };
 
 console.log(insertIntoBST(root, 5));
