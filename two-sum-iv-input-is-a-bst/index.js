d1e7b46c075604c23627536f7b088230b591b9b3
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-09-15 00:43:04 +0530

    2 sum 4

--- two-sum-iv-input-is-a-bst/index.js
+++ two-sum-iv-input-is-a-bst/index.js
@@ -10,7 +10,6 @@ const node6 = new TreeNode(6)
 const node2 = new TreeNode(2)
 const node4 = new TreeNode(4)
 const node7 = new TreeNode(7)
-
 root.left = node3
 root.right = node6
 node3.left = node2
@@ -29,6 +28,48 @@ var findTarget = function (root, k) {
 
         current.right && stack.push(current.right)
         current.left && stack.push(current.left)
+    }
+    return false
+}
+
+var findTarget = function (root, k) {
+    class BSTIterator {
+        constructor(root, isLeft) {
+            this.stack = [];
+            this.isLeft = isLeft;
+            this.pushAll(root);
+        }
+
+        next() {
+            const current = this.stack.pop();
+            if (this.isLeft) {
+                this.pushAll(current.right);
+            } else {
+                this.pushAll(current.left);
+            }
+            return current.val;
+        }
+
+        pushAll(node) {
+            while (node) {
+                this.stack.push(node);
+                node = this.isLeft ? node.left : node.right;
+            }
+        }
+    }
+
+    if (!root) return false;
+
+    const b1 = new BSTIterator(root, true);
+    const b2 = new BSTIterator(root, false);
+
+    let left = b1.next();
+    let right = b2.next();
+
+    while (left < right) {
+        const sum = left + right
+        if (sum === k) return true;
+        sum < k ? left = b1.next() : right = b2.next()
     }
     return false
 }
