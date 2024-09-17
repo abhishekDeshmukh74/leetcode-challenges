70fd9a67893c0ded13a59d762e770292da9ff68d
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-09-17 22:47:54 +0530

    PP2 updated

--- palindrome-partitioning-ii/index.js
+++ palindrome-partitioning-ii/index.js
@@ -1,34 +1,47 @@
-// Time Complexity: O(N^2⋅N)
+const isPalindrome = (s, i, j, dpPal) => {
+    if (i >= j) return true;
+    if (dpPal[i][j] !== undefined) return dpPal[i][j];
+    if (s[i] === s[j]) return dpPal[i][j] = isPalindrome(s, i + 1, j - 1, dpPal);
+    return dpPal[i][j] = false;
+};
+
 var minCut = function (s) {
+    const dpPal = Array(s.length).fill().map(() => Array(s.length).fill())
+    const dp = Array(s.length).fill()
 
-    const memoPalindrome = Array(s.length).fill().map(() => Array(s.length).fill(null))
-    const isPalindrome = (i, j) => {
-        if (memoPalindrome[i][j]) return memoPalindrome[i][j];
-        if (i >= j) return true;
-        if (s[i] === s[j]) {
-            memoPalindrome[i][j] = isPalindrome(i + 1, j - 1);
-        } else {
-            memoPalindrome[i][j] = false;
-        }
-        return memoPalindrome[i][j];
-    };
+    const dfs = (i) => {
+        if (i === s.length) return 0;
 
-    const memo = Array(s.length).fill().map(() => Array(s.length).fill(0))
-    const dfs = (i, j) => {
-        // case '' && 'a'
-        if (j <= i || isPalindrome(i, j)) return 0
-        if (memo[i][j]) return memo[i][j]
+        if (dp[i] !== undefined) return dp[i]
 
         let min = Infinity
-        for (let k = i; k < j; k++) {
-            // only cut of str(i,k) is palindrome
-            if (isPalindrome(i, k)) {
-                min = Math.min(min, 1 + dfs(i, k) + dfs(k + 1, j))
+        for (let j = i; j < s.length; j++) {
+            if (isPalindrome(s, i, j, dpPal)) {
+                const cost = 1 + dfs(j + 1);
+                min = Math.min(min, cost);
             }
         }
-        return memo[i][j] = min
+        return dp[i] = min
     }
-    return dfs(0, s.length - 1)
+    // to adjust for the extra cut added in the last index
+    return dfs(0) - 1
+};
+
+var minCut = function (s) {
+    const dpPal = Array(s.length).fill().map(() => Array(s.length).fill());
+    const dp = Array(s.length + 1).fill(0)
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        let min = Infinity
+        for (let j = i; j < s.length; j++) {
+            if (isPalindrome(s, i, j, dpPal)) {
+                const cost = 1 + dp[j + 1];
+                min = Math.min(min, cost);
+            }
+        }
+        dp[i] = min
+    }
+    return dp[0] - 1
 };
 
 console.log(minCut('aab'))
