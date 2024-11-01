ac4ce2cdbaaf687c2335262887cd920e63bcd351
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2024-11-01 19:28:18 +0530

    romanToInt

---
+++ roman-to-integer/index.js
@@ -0,0 +1,31 @@
+var romanToInt = function (s) {
+    const romanNumerals = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let sum = 0;
+    for (let i = 0; i < s.length; i++) {
+        const current = romanNumerals[s[i]];
+        const next = romanNumerals[s[i + 1]];
+
+        if (next && current < next) {
+            sum += next - current;
+            i++;
+        } else {
+            sum += current;
+        }
+    }
+    return sum;
+};
+
+console.log(romanToInt("I"))
+console.log(romanToInt("III"))
+console.log(romanToInt("IV"))
+console.log(romanToInt("LVIII"))
+console.log(romanToInt("MCMXCIV"))
