054627a98205bfb1354380a84b4865df259536d6
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2025-08-14 11:39:41 +0530

    merge sort sortArray

--- sort-an-array/index.js
+++ sort-an-array/index.js
@@ -1,4 +1,5 @@
-const sortArray = (nums, left = 0, right = nums.length - 1) => {
+// Quick sort
+var sortArray = (nums, left = 0, right = nums.length - 1) => {
     if (nums.length < 2) return nums
     if (left >= right) return
 
@@ -22,6 +23,56 @@ const sortArray = (nums, left = 0, right = nums.length - 1) => {
     return nums
 }
 
+// Merge sort
+var sortArray = (nums) => {
+
+    const sort = (low, high) => {
+        if (low >= high) return;
+
+        const mid = Math.floor((low + high) / 2)
+
+        sort(low, mid);
+        sort(mid + 1, high);
+        merge(low, mid, high);
+    }
+
+    const merge = (low, mid, high) => {
+
+        const sorted = [];
+
+        let left = low;
+        let right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                sorted.push(nums[left]);
+                left++;
+            } else {
+                sorted.push(nums[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            sorted.push(nums[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            sorted.push(nums[right]);
+            right++;
+        }
+
+        for (let i = low; i <= high; i++) {
+            nums[i] = sorted[i - low];
+        }
+    }
+
+    sort(0, nums.length - 1)
+    return nums;
+}
+
 console.log(sortArray([0]))
 console.log(sortArray([5, 2, 3, 1]))
 console.log(sortArray([5, 1, 1, 2, 0, 0]))
+console.log(sortArray([-2, 3, -5]))
